94eb7a2314ff3f1501fdf3b278e95e133109e92a
Arash Ghalebi <aghalebi33@gmail.com>
2022-02-15 13:39:41 -0500

    Complete AddWeightEntry component

--- src/pages/weight-tracker/WeightTracker.jsx
+++ src/pages/weight-tracker/WeightTracker.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import DisplayWeightEntries from '../../Components/DisplayWeightEntries/DisplayWeightEntries';
+import AddWeightEntry from '../../Components/AddWeightEntry/AddWeightEntry';
 
 export default function WeightTracker() {
 
     const [entries, setEntries] = useState([])
 
+    function addNewEntry(entry){
+        let tempEntries = [entry, ...entries];
+        setEntries(tempEntries);
+    }
+
     return (
         <div>
             <DisplayWeightEntries parentEntries={entries} />
+            <AddWeightEntry addNewEntry={addNewEntry}/>
         </div>
     );
 
